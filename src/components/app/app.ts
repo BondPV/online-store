107f2223f5ee23e5f88d1abb33dd667cae4732bf
BondPV <bondpv@yandex.ru>
2022-12-14 16:24:26 +0300

    feat: products database

---
+++ src/components/app/app.ts
@@ -0,0 +1,7 @@
+class App {
+  public start() {
+    alert('start task');
+  }
+}
+
+export default App;
---
+++ src/components/controller/controller.ts
@@ -0,0 +1,6 @@
+import productJSON from '../../database/database.json';
+import { IProducts } from '../types/types';
+
+class AppController {}
+
+export default AppController;
