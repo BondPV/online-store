13a448c21e77cf50eea3b2404b4acb7540638587
Alesia-V175 <vaivasilisa@gmail.com>
2022-12-19 17:04:06 +0300

    feat: add stuly of catalog

--- src/components/main/product/Product.ts
+++ src/components/main/product/Product.ts
@@ -5,14 +5,18 @@ class Product {
   constructor(private product: IProduct) {}
 
   render() {
+    const discount = 1 + this.product.discountPercentage / 100;
+    const initialPrice: number = Math.round(this.product.price * discount);
     return `
       <div class="product">
         <div class="product__stock">stock: ${this.product.stock}</div>
         <div class="product__img-wrap">
-        <img class="product__img"src="${this.product.images[0]}" alt="${this.product.title}">
+          <div class="product__img" style="background: url('${this.product.images[0]}') no-repeat center / cover;"></div>
         </div>
         <div class="product__title">${this.product.title}</div>
-        <div class="product__price">${this.product.price}$</div>
+        <div class="product__description">${this.product.description}</div>
+        <div class="product__price">${initialPrice}$</div>
+        <div class="product__price_discount">${this.product.price}$</div>
       </div>
       `;
   }
