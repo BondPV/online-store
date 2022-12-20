1ad10efd4a17a0f17567355eb6e4c56ecb250338
Alesia-V175 <vaivasilisa@gmail.com>
2022-12-20 12:59:48 +0300

    feat: add some code for sorting

--- src/components/main/catalog/Catalog.ts
+++ src/components/main/catalog/Catalog.ts
@@ -1,10 +1,24 @@
 //  product catalog implementation
+
 import { IdMap } from 'constants/htmlConstants';
+import ProductsDB from 'database/ProductsDB';
 import Product from 'components/main/product/Product';
 import { IProduct } from 'types/interfaces';
 import './catalog.scss';
 
 class Catalog {
+  private products: IProduct[] = [];
+
+  constructor() {
+    this.initProducts();
+  }
+
+  //  product database initialization
+  initProducts() {
+    this.products = ProductsDB.getProducts();
+  }
+
+
   static render(catalog: IProduct[]) {
     const parentElement = document.querySelector(IdMap.catalog);
     const products: string = catalog
@@ -16,6 +30,37 @@ class Catalog {
       parentElement.innerHTML = products.length ? products : 'No products found';
     }
   }
+  // render(sort = null) {
+  //   let listProduct = this.products;
+  //   if(sort) {
+  //     // listProduct = this.sort(sort, listProduct);
+  //   }
+  //   const products: string = listProduct
+  //     .map((product) => new Product(product))
+  //     .map((product) => product.render())
+  //     .join('');
+  //   return `
+  //   <div class="catalog__header">
+  //       <div class="catalog__sort-wrap">Sort BY
+  //         <select>
+  //           <option>Name ASC</option>
+  //           <option>Name DESC</option>
+  //           <option>Price ASC</option>
+  //           <option>Price DESC</option>
+  //           <option>Rating ASC</option>
+  //           <option>Rating DESC</option>
+  //         </select>
+  //       </div>
+  //       <div class="catalog__view catalog__view-list">
+  //         <i class="fa-solid fa-table-cells"></i>
+  //       </div>
+  //       <div class="catalog__view catalog__view-table">
+  //         <i class="fa-solid fa-list"></i>
+  //       </div>
+  //   </div>
+  //   <div class="catalog">
+  //   ${products}</div>`;
+  // }
 }
 
 export default Catalog;
