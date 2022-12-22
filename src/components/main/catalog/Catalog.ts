4c59a79a4f24dbe06487c5c532d1de9e92173443
Alesia-V175 <vaivasilisa@gmail.com>
2022-12-22 17:48:42 +0300

    feat: implement grid page, add 1 point of sorting

--- src/components/main/catalog/Catalog.ts
+++ src/components/main/catalog/Catalog.ts
@@ -18,6 +18,88 @@ class Catalog {
     this.products = ProductsDB.getProducts();
   }
 
+  appendSortWrap(patentElement: Element): void  {
+    const catalogHeader: HTMLElement = document.createElement('div');
+    catalogHeader.classList.add('catalog__header');
+    patentElement.prepend(catalogHeader);
+
+    // sort
+    const selectSortButton: HTMLSelectElement = document.createElement('select');
+    catalogHeader.append(selectSortButton);
+
+    selectSortButton.addEventListener('change', () => {
+      console.log(selectSortButton.value);
+      const currentOptionValue = selectSortButton.options[selectSortButton.selectedIndex];
+      currentOptionValue.selected = true;
+      this.sortCatalog();
+      const oldCatalog = document.querySelector('.catalog');
+      if (oldCatalog && oldCatalog.parentNode) {
+        oldCatalog.parentNode.removeChild(oldCatalog);
+      }
+      const main = document.querySelector('.catalog-products');
+      if (main && main instanceof HTMLElement) {
+        main.innerHTML = this.render();;
+      }
+      //remove old catalog
+      // this.render();
+    })
+
+    const optionNames = ['Name ASC', 'Name DESC', 'Price ASC', 'Price DESC', 'Rating ASC', 'Rating DESC'];
+
+    optionNames.forEach((item) => {
+      this.createOptionsSort(item, selectSortButton);
+    })
+
+
+    // grid view
+
+    const viewButtonTable: HTMLDivElement = document.createElement('div');
+    viewButtonTable.classList.add('catalog__view', 'catalog__view-table');
+    catalogHeader.append(viewButtonTable);
+
+    const iconButtonTable: HTMLElement = document.createElement('i');
+    iconButtonTable.classList.add('fa-solid', 'fa-table-cells');
+    viewButtonTable.append(iconButtonTable);
+
+    const viewButtonList: HTMLDivElement = document.createElement('div');
+    viewButtonList.classList.add('catalog__view', 'catalog__view-list');
+    catalogHeader.append(viewButtonList);
+
+    const iconButtonList: HTMLElement = document.createElement('i');
+    iconButtonList.classList.add('fa-solid', 'fa-list');
+    viewButtonList.append(iconButtonList);
+
+    viewButtonList.addEventListener('click', () => {
+      const catalogView = document.querySelector('.catalog');
+      const productCard = document.querySelector('.product');
+      if (catalogView && productCard) {
+        catalogView.classList.add('catalog_list');
+        productCard.classList.add('product_list');
+      }
+    })
+
+    viewButtonTable.addEventListener('click', () => {
+      const catalogView = document.querySelector('.catalog');
+      const productCard = document.querySelector('.product');
+      if (catalogView && productCard) {
+        catalogView.classList.remove('catalog_list');
+        productCard.classList.remove('product_list');
+      }
+    })
+  }
+
+  sortCatalog() {
+    this.products = this.products.sort((prevValue, nextValue) => {
+      return prevValue.price < nextValue.price ? -1 : 1;
+    })
+  }
+
+  createOptionsSort(optionName: string, parentElem: HTMLSelectElement) {
+    const optionSort: HTMLOptionElement = document.createElement('option');
+    optionSort.textContent = optionName;
+    optionSort.value = optionName;
+    parentElem.append(optionSort);
+  }
 
   static render(catalog: IProduct[]) {
     const parentElement = document.querySelector(IdMap.catalog);
@@ -25,6 +107,11 @@ class Catalog {
       .map((product) => new Product(product))
       .map((product) => product.render())
       .join('');
+
+    const catalogSort = document.querySelector('.catalog-sort');
+    if(catalogSort) {
+      this.appendSortWrap(catalogSort);
+    }
 
     if (parentElement instanceof HTMLElement) {
       parentElement.innerHTML = products.length ? products : 'No products found';
