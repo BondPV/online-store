a4dd20852a79ec08cbfb8f5d793d53e29f649fd1
BondPV <bondpv@yandex.ru>
2023-01-12 13:03:46 +0300

    feat: add jest tests settings

---
+++ src/tests/UrlHashCart.test.ts
@@ -0,0 +1,7 @@
+import UrlHashCart from 'helpers/router/UrlHashCart';
+
+describe('UrlHashCart: hashCartData', () => {
+  test('should be defined', () => {
+    expect(UrlHashCart.hashCartData).toBeDefined();
+  });
+});
